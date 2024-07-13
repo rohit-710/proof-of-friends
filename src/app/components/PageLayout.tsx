8ba58ac8c9ab291acefc28732dc971398191481f
Rohit Ramesh <rrohitramesh710@gmail.com>
2024-07-13 19:32:48 +0200

    UI changes

---
+++ src/app/components/PageLayout.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+const PageLayout = ({ title }: { title?: string }) => {
+  return (
+    <>
+      <link rel="icon" type="image/x-icon" href="/favicon.jpeg" />
+      <title>
+        {title ? `${title} | ` : ""}
+        Proof of Friends
+      </title>
+
+      <div className="pt-10 mb-10 grid place-items-center">
+        <Link href="/" passHref>
+          <img src="/chainscout.svg" alt="" />
+        </Link>
+
+        <p className="mx-2 text-xl mt-10 h-20 text-white">{title}</p>
+      </div>
+    </>
+  );
+};
+
+export default PageLayout;
